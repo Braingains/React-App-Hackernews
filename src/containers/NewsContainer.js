23bb71359373ff4ed524fb91446d6a625cadcb70
Ewan Brain <ewan.brain@gmail.com>
2021-03-03 11:21:34 +0000

    api call working

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import StoriesList from '../components/StoriesList';
 
 const NewsContainer = () => {
+
+    const [topStories, setTopStories] = useState([]);
+    const [stories, setStories] = useState([]);
+
+    useEffect(() => {
+        loadTopStories()
+    }, [])
+
+    useEffect(() => {
+        loadStory()
+    },[topStories])
+
+    const loadTopStories = () => {
+        fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
+        .then(res => res.json())
+        .then(data => setTopStories(data))
+    }
+
+    const loadStory = () => {
+        fetch(`https://hacker-news.firebaseio.com/v0/item/${topStories[1]}.json`)
+        .then(res => res.json())
+        .then(data => setStories(data))
+    }
+
+
+
+
     return (
         <>
         <p>I'm the newsContainer</p>
@@ -11,3 +38,7 @@ const NewsContainer = () => {
 }
 
 export default NewsContainer;
+
+
+// https://hacker-news.firebaseio.com/v0/topstories.json 
+// https://hacker-news.firebaseio.com/v0/item/{storyId}.json
