aadd9491ee2717fbb27ee84eff744e0860fa9afe
Ewan Brain <ewan.brain@gmail.com>
2021-03-03 12:00:17 +0000

    promise.all comments added

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -16,14 +16,20 @@ const NewsContainer = () => {
 
     const loadTopStories = () => {
         fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
+        //we fetch the array of article numbers
         .then(res => res.json())
         .then(data => {
             const slicedData = data.slice(0, 20)
+            //we slice the array to only return the first 20
             const ipromise = slicedData.map(storyId => {
+                //we map those 20 into a new array
                 return fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+                //this array is of the above url with the article numbers interpolated
+                //so this is an array of fetch requests
                 .then(res => res.json())
             });
             Promise.all(ipromise).then(data => setStories(data))
+            //Promise.all waits for all of the data to come back before adding it to setStories
         })
     }
 
